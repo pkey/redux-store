6b8be7702fbf71abdbb67fb211924df99449111e
Paulius Kutka <kutka.paulius@gmail.com>
2019-05-24 14:25:17 +0300

    Finished tutorial in a few hours :alarm_clock:

---
+++ src/store/actions.ts
@@ -0,0 +1,15 @@
+//action constants
+export const ADD_TODO = "[TODO] Add Todo";
+export const REMOVE_TODO = "[TODO] Remove Todo";
+//action creators
+export class AddTodo {
+  readonly type = ADD_TODO;
+  constructor(private payload: any) {}
+}
+
+export class RemoveTodo {
+  readonly type = REMOVE_TODO;
+  constructor(private payload: any) {}
+}
+
+console.log(new AddTodo({}));
